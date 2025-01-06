db7138b3be7704620ca1ecdd6fa9eb14edd3f5c0
codejedi365 <codejedi365@gmail.com>
2025-01-05 23:43:11 -0700

    chore(srvc1-scripts): add release automation script

---
+++ scripts/release-srvc1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+PROJECT_ROOT="$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
+VIRTUAL_ENV="$PROJECT_ROOT/.venv"
+
+SERVICE_NAME="srvc1"
+
+cd "$PROJECT_ROOT" || exit
+
+# Setup documentation template
+pushd "documentation/templates" >/dev/null || exit
+
+rm -rf documentation/
+mkdir -p "documentation/docs/"
+cp -r .base_changelog_template/ "documentation/docs/$SERVICE_NAME"
+
+popd >/dev/null || exit
+
+# Release the service
+pushd "poweralpha/services/$SERVICE_NAME" >/dev/null || exit
+
+printf '%s\n' "Releasing $SERVICE_NAME..."
+"$VIRTUAL_ENV/bin/semantic-release" -v version --no-push
+# "$VIRTUAL_ENV/bin/semantic-release" -v changelog
+
+popd >/dev/null || exit
